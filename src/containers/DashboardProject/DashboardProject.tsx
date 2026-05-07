ce9d142ee962979dd779b4b83fee4f135a78f146
ValKer-OPCS <icyfry@gmail.com>
2026-05-07 11:13:24 +0200

    Add DashboardProject container
    Create a new DashboardProject container at src/containers/DashboardProject/DashboardProject.tsx that imports and renders AdminProjectUploader and exports the component as default. Provides a simple scaffold to host the project uploader in the dashboard.

---
+++ src/containers/DashboardProject/DashboardProject.tsx
@@ -0,0 +1,12 @@
+import AdminProjectUploader from '@/components/AdminProjectUploader/AdminProjectUploader'
+
+
+const DashboardProject = () => {
+  return (
+    <div>
+      <AdminProjectUploader/>
+    </div>
+  )
+};
+
+export default DashboardProject
