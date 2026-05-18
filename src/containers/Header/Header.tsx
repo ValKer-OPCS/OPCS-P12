20771e3668b578a49d4a2857668d7c516cca7c35
ValKer-OPCS <icyfry@gmail.com>
2026-05-18 09:46:26 +0200

    Replace header nav with NavBar component
    Import and use the shared NavBar component in Header.tsx, removing the previous inline <nav> and list markup. Cleaned up formatting (added semicolons, adjusted export placement) in Header.tsx. Removed the .navBar rules from style.module.scss and adjusted .headerContainer formatting. Purpose: centralize navigation markup/styling by reusing the NavBar component and simplify the header component.

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,28 +1,19 @@
-import styles from './style.module.scss'
-import Link from 'next/link'
-
+import styles from './style.module.scss';
+import Link from 'next/link';
 
 import NamePlate from '../../components/NamePlate/NamePlate';
-
-
+import NavBar from '@/components/NavBar/NavBar';
 
 const Header = () => {
 
   return (
-
     <header data-testid="header" className={styles.headerContainer}>
-      <Link href={"/"}> <NamePlate /> </Link>
-      <nav>
-        <ul className={styles.navBar}>
-          <li><a href="#about">A propos</a></li>
-          <li><a href="#projects">Projets</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+      <Link href={"/"}><NamePlate /></Link>
+
+      <NavBar />
+
     </header>
-  )
+  );
 };
 
-export default Header
-
-
+export default Header;
