3fe0944832c0478db0c4a4eb4606d53df3356ad1
ValKer-OPCS <icyfry@gmail.com>
2026-05-18 09:47:58 +0200

    Add NavBar component with admin modal
    Introduce a client-side NavBar component (NavBar.tsx) and accompanying SCSS module. The NavBar uses Next.js navigation hooks to detect dashboard routes and conditionally renders public anchors or dashboard actions (Add Project opens AdminModalUploader, Logout clears token from localStorage and redirects to "/"). Add style.module.scss for basic nav styling and hover behavior.

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { usePathname, useRouter } from 'next/navigation';
+import styles from "./style.module.scss";
+import { useState } from 'react';
+import AdminModalUploader from '../../components/AdminModalUploader/AdminModalUploader';
+
+const NavBar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+  const isDashboard = pathname.startsWith("/dashboard");
+
+    const handleLogout = () => {
+    localStorage.removeItem("token");
+    router.push("/");
+  };
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+    <nav>
+      <ul className={styles.navBar}>
+        {!isDashboard ? (
+          <>
+            <li><a href="#about">A propos</a></li>
+            <li><a href="#projects">Projets</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </>
+        ) : (
+          <>
+            <li><button onClick={() => setIsModalOpen(true)}> Ajouter un projet </button></li>
+            <li><button onClick={handleLogout}>Déconnexion</button></li>
+          </>
+        )}
+      </ul>
+    </nav>
+
+          {isModalOpen && (
+        <AdminModalUploader onClose={() => setIsModalOpen(false)} />
+      )}
+      </>
+  );
+};
+
+export default NavBar;
